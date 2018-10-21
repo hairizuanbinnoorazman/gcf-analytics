3586607a13426a43f924d43a50e5b88aef70a081
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-10-21 22:48:05 +0800

    feat: Add initial cloud function for testing

---
+++ main.py
@@ -0,0 +1,9 @@
+def main(data, context):
+    """Background Cloud Function to be triggered by Cloud Storage.
+    Args:
+         data (dict): The dictionary with data specific to this type of event.
+         context (google.cloud.functions.Context): The Cloud Functions
+         event metadata.
+    """
+    print(data)
+    print(context)
