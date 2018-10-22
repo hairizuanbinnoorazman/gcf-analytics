6b2f3bbbc1dc341501ed9310f605a0f65671112e
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-10-22 23:15:57 +0800

    fix: Added sample datasets that can be used for demo

--- test_analytics_check.py
+++ test_analytics_check.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import analytics_check
 
-def test_run_check():
+
+def test_run_incorrect_check():
     test_dataset1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
     errlist = analytics_check.run_check(test_dataset1)
     assert len(errlist) == 5, "Wrong input provided"
+
+
+def test_run_correct_file_check():
+    data = pd.read_csv("test_correct_data.csv")
+    errlist = analytics_check.run_check(data)
+    assert len(errlist) == 0, "No errors expected from the csv"
+
+
+def test_run_incorrect_file_check():
+    data = pd.read_csv("test_incorrect_data.csv")
+    errlist = analytics_check.run_check(data)
+    assert len(errlist) == 1, "1 error expected from this"
